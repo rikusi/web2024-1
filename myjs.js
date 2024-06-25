d1d71f1ba5f1546b30296338ef68a92669f78e57
rikusi <167832611+rikusi@users.noreply.github.com>
2024-06-25 11:30:06 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -2,10 +2,10 @@ alert('Hello, Rikuto!');
 alert(1+1);
 alert(Math.sqrt(1+1));
 alert(Math.sqrt(2)Math.sqrt(3));
-alert(1/0)
-alert(0/0)
+alert(1/0);
+alert(0/0);
 alert(2+3);
 alert('2'+'3');
 alret(Number('2')+Number('3'));
-alret('2'+3)
-alert(Number('2')+3)
+alret('2'+3);
+alert(Number('2')+3);
